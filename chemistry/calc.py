14e7d1c05f2b4ccac652101be946b1f818aca81d
Gvit0 <90312305+Gvit0@users.noreply.github.com>
2024-04-25 11:45:38 +0300

    Update calc.py add func

--- chemistry/calc.py
+++ chemistry/calc.py
@@ -3,25 +3,34 @@ import mend_table as MT
 
 
 def TextToFormul(text):
-	pass
+		pass
 def FindMolMass(formul):
-	molMassT = 0
-	for formulObj in formul:
-		for obj in formulObj:
-			if obj in MT.funcSumbol:
-				molMass = "Поиск молярной массы в формуле не временно поддерживается"
-			else:
-				Fobj = ""
-				FobjIndex = ""
-				for sumbol in obj:
-					
-					if sumbol in MT.upReg or sumbol in MT.downReg:
-						Fobj += sumbol
-					elif sumbol in MT.num:
-						FobjIndex += sumbol
-				if FobjIndex == "":
-					FobjIndex = "1"
-				MTobj = MT.get_use_name(Fobj)
-				molMassT +=float(MTobj["atom_mass"]) * int(FobjIndex)
-	molMass = str(molMassT)
-	return molMass
+		molMassT = 0
+		for obj in formul:
+			Fobj = ""
+			FobjIndex = ""
+			for sumbol in obj:
+				if sumbol in MT.upReg or sumbol in MT.downReg:
+					Fobj += sumbol
+				elif sumbol in MT.num:
+					FobjIndex += sumbol
+			if FobjIndex == "":
+				FobjIndex = "1"
+			MTobj = MT.get_use_name(Fobj)
+			molMassT +=float(MTobj["atom_mass"]) * int(FobjIndex)
+		molMass = str(molMassT)
+		return molMass
+		
+def PercentageByMass(formul,objF):
+	objP = ""
+	
+	for obj in formul:
+		objP = ""
+		for objT in obj:
+			if objT not in MT.num:
+				objP+= objT
+		if objP == objF:
+			objP = obj
+			break
+	percentage =(float(FindMolMass([objP]))/ float(FindMolMass(formul)))*100
+	return percentage
