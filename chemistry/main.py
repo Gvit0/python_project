b36f71a07236d22d6f9b80927e6c4a78ceea69fd
Gvit0 <90312305+Gvit0@users.noreply.github.com>
2024-04-25 11:44:57 +0300

    Update main.py add func and info

--- chemistry/main.py
+++ chemistry/main.py
@@ -2,25 +2,42 @@ import mend_table as MT
 import handler
 import calc
 from time import sleep
+
 print("Добро пожаловать в химический калькулятор!")
 
 def PrintMenu():
 	print("возможности:")
 	print("1)Найти молярную массу")
-	#print("2) формула в текстовую ")
-	#print("3) название формулы")
-	print("q-выход")
-
+	print("2)Уравнять-")
+	print("3)Найти массовую долю")
+	print("q- Выход")
+	print("h- информация")
+	print("")
+def printInfo():
+	print("Формулы ввод : ClHO2+H2O=Cl")
+	print("почта для ошибок: gatvit@mail.ru")
+	print("версия:")
+	print("для обновления:")
+	print("")
 def MenuInput(InMen):
 	if InMen == "q":
 		exit()
+	elif InMen == "h":
+		printInfo()
 	elif int(InMen)== 1:
-		print(f"Молярная масса: {calc.FindMolMass(handler.FormulToMass(input("Введите формулу для поиска молярной массы: ")))}")
+		print(f"Молярная масса: {calc.FindMolMass(handler.FormulToMass(input('Формула:'))[0])}")
+	elif int(InMen)==2:
+		formul = handler.FormulToMass(input('Формула:'))
+	
+	elif int(InMen)==3:
+		formul = handler.FormulToMass(input('Формула: '))[0]
+		objF =input('Найти долю ')
+		otvet = calc.PercentageByMass(formul,objF)
+		print(f"Ответ: {otvet}%")
 	elif int(InMen) ==0:
 		print(handler.FormulToMass(input("формула:")))
 		
 while True:
 	PrintMenu()
 	MenuInput(input("Номер меню: "))
-	sleep(3)
-	
+	sleep(7)
